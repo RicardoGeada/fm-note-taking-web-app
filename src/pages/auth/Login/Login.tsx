a40b00cedc965f71bd159651abe41eedbed4b517
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-07-28 13:55:46 +0200

    feat: add basic HTML structure

--- src/pages/auth/Login/Login.tsx
+++ src/pages/auth/Login/Login.tsx
@@ -1,7 +1,37 @@
 import "./Login.module.scss";
 
 function Login() {
-  return <>Login</>;
+  return (<main>
+    <form action="">
+        <img src="./images/logo.svg" alt="notes logo" />
+        <h1>Welcome to Note</h1>
+        <p>Please log in to continue</p>
+
+        <label htmlFor="email">Email Adress</label>
+        <input id="email" type="email" placeholder="email@example.com"/>
+
+        <label htmlFor="password">Password</label>
+        <a href="">Forgot</a>
+        <input id="password" type="password" />
+
+        <button type="submit">Login</button>
+
+        <div className="hl-separator"></div>
+
+        <p>Or log in with:</p>
+        <button>
+            <img src="./images/icon-google.svg" alt="" />
+            Google
+        </button>
+
+        <div className="hl-separator"></div>
+
+        <p>
+          <span>No account yet?</span>  
+          <a href="">Sign Up</a>  
+        </p>        
+    </form>
+    </main>);
 }
 
 export default Login;
