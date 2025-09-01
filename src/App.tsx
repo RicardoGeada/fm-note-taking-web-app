075301875fb212c8e7b375ff914615a7352b261b
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-09-01 13:56:16 +0200

    feat: add HTML & CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // import Home from './pages/main/Home/Home';
 import Login from './pages/auth/Login/Login';
 import ForgotPassword from './pages/auth/ForgotPassword/ForgotPassword';
+import ResetPassword from './pages/auth/ResetPassword/ResetPassword';
 
 const router = createBrowserRouter([
   {path: '/', element: <Login />},
   {path: '/login', element: <Login />},
   {path: '/forgot-password', element: <ForgotPassword />},
+  {path: '/reset-password', element: <ResetPassword />},
 ]);
 
 function App() {
