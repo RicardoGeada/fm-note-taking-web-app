f217ea8a41904e308dda902fe23babd524d7e979
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-09-18 10:52:08 +0200

    feat(notes-list): add note list, add note list item

--- src/pages/main/AllNotes/AllNotes.tsx
+++ src/pages/main/AllNotes/AllNotes.tsx
@@ -1,9 +1,14 @@
+import NoteListItem from "../../../components/NoteListItem/NoteListItem";
 import styles from "./AllNotes.module.scss";
+import DUMMY_NOTES from "../../../dummy-notes";
 
 export default function AllNotes() {
   return (
     <div className={styles["all-notes"]}>
       <h2 className={styles["all-notes__headline"]}>All Notes</h2>
+      <ul className={styles["notes-list"]}>
+        {DUMMY_NOTES.map( note => <NoteListItem key={note.id} {...note}/>)}
+      </ul>
     </div>
   );
 }
