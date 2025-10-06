7450efab14db85118e91f8ab94c9fa7baba5c964
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-10-06 13:56:34 +0200

    feat(desktop-layout): add basic layout

--- src/layouts/Desktop/DesktopLayout.tsx
+++ src/layouts/Desktop/DesktopLayout.tsx
@@ -1,5 +1,11 @@
+import styles from "./DesktopLayout.module.scss";
+
 export default function DesktopLayout() {
     return (
-        <>Desktop Layout</>
+        <div className={styles["layout"]}>
+            <header className={styles["layout__header"]}>DesktopPageHeader</header>
+            <main className={styles["layout__main"]}>DesktopMainContent</main>
+            <nav className={styles["layout__nav"]}>DesktopNav</nav>
+        </div>
     )
 }
