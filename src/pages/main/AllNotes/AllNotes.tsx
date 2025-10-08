72111d4cdad777175bbf14e49ece5ecf7212c120
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-10-08 15:58:47 +0200

    feat(all-notes): add desktop layout
    - add css for all-notes in desktop layout

--- src/pages/main/AllNotes/AllNotes.tsx
+++ src/pages/main/AllNotes/AllNotes.tsx
@@ -4,19 +4,24 @@ import DUMMY_NOTES from "../../../dummy-notes";
 import { useParams } from "react-router-dom";
 import NoteDetail from "../../../components/NoteDetail/NoteDetail";
 import AddIcon from "./../../../assets/images/icon-plus.svg?react";
+import { useMediaQuery } from "react-responsive";
+import clsx from "clsx";
 
 export default function AllNotes() {
+  const isDesktop = useMediaQuery({ minWidth: 1080 });
   const { noteId } = useParams();
 
   return (
     <>
       <div className={styles["all-notes"]}>
-        <h2 className={styles["all-notes__headline"]}>All Notes</h2>
+        {!isDesktop && (
+          <h2 className={styles["all-notes__headline"]}>All Notes</h2>
+        )}
 
-        <button className={styles["new-note-button"]}>
-          <AddIcon />
+        <button className={clsx("btn btn--primary",styles["new-note-button"], isDesktop ? "" : "btn--circle")}>
+          {isDesktop ? <span>+ Create New Note</span> : <AddIcon />}
         </button>
-        
+
         {DUMMY_NOTES.length > 0 && (
           <ul className={styles["notes-list"]}>
             {DUMMY_NOTES.filter((note) => !note.archived).map((note) => (
