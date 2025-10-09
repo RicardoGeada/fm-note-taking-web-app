ab7dccbad360649de37f5438964ac516011e0e65
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-10-09 21:28:07 +0200

    refactor: add noteslist component from all notes

--- src/pages/main/AllNotes/AllNotes.tsx
+++ src/pages/main/AllNotes/AllNotes.tsx
@@ -1,47 +1,15 @@
-import NoteListItem from "../../../components/NoteListItem/NoteListItem";
 import styles from "./AllNotes.module.scss";
-import DUMMY_NOTES from "../../../dummy-notes";
 import { Outlet, useParams } from "react-router-dom";
-import AddIcon from "./../../../assets/images/icon-plus.svg?react";
 import { useMediaQuery } from "react-responsive";
-import clsx from "clsx";
+import NotesList from "../../../components/NotesList/NotesList";
 
 export default function AllNotes() {
   const isDesktop = useMediaQuery({ minWidth: 1080 });
   const { noteId } = useParams();
 
   return (
-    <section className={styles["all-notes-section"]}>
-      <div className={styles["all-notes"]}>
-        {!isDesktop && (
-          <h2 className={styles["all-notes__headline"]}>All Notes</h2>
-        )}
-
-        <button
-          className={clsx(
-            "btn btn--primary",
-            styles["new-note-button"],
-            isDesktop ? "" : "btn--circle"
-          )}
-        >
-          {isDesktop ? <span>+ Create New Note</span> : <AddIcon />}
-        </button>
-
-        {DUMMY_NOTES.length > 0 && (
-          <ul className={styles["notes-list"]}>
-            {DUMMY_NOTES.filter((note) => !note.archived).map((note) => (
-              <NoteListItem key={note.id} {...note} />
-            ))}
-          </ul>
-        )}
-
-        {DUMMY_NOTES.length === 0 && (
-          <div className={styles["all-notes__empty-notification"]}>
-            You don't have any notes yet. Start a new note to capture your
-            thoughts and ideas.
-          </div>
-        )}
-      </div>
+    <div className={styles["all-notes"]}>
+      <NotesList />
 
       {!isDesktop && noteId && (
         <div className={styles["overlay"]}>
@@ -50,6 +18,6 @@ export default function AllNotes() {
       )}
 
       {isDesktop && noteId && <Outlet />}
-    </section>
+    </div>
   );
 }
