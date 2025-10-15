6ffa6b3869d6278cc25c026b6da7259940f0a0ab
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-10-15 19:45:16 +0200

    feat(tags): add taglist for mobile

--- src/pages/main/Tags/Tags.tsx
+++ src/pages/main/Tags/Tags.tsx
@@ -2,7 +2,9 @@ import { useMediaQuery } from "react-responsive";
 import { Outlet, useParams } from "react-router-dom";
 import styles from "./Tags.module.scss";
 import NotesList from "../../../components/NotesList/NotesList";
-import { DUMMY_NOTES } from "../../../dummy-notes";
+import { TAGS , DUMMY_NOTES } from "../../../dummy-notes";
+import SidebarItem from "../../../components/SidebarItem/SidebarItem";
+import TagIcon from "../../../assets/images/icon-tag.svg?react";
 
 export default function Tags() {
   const isDesktop = useMediaQuery({ minWidth: 1080 });
@@ -10,18 +12,29 @@ export default function Tags() {
   const taggedNotes = tagId ? DUMMY_NOTES.filter((note) => note.tags.includes(tagId)) : [];
   return (
     <>
-      {(!isDesktop && !tagId) && <>Tag Liste</>}
-      {(!isDesktop && tagId) && <>Notes Liste mit {tagId}</>}
-      {(isDesktop && tagId) && 
+      {(!isDesktop && !tagId) && <div className={styles["tagged-notes"]}>
+        <div className={styles["tag-list-wrapper"]}>
+          <h1 className={styles["tag-list__headline"]}>Tags</h1>
+          <ul className={styles["tag-list"]}>
+          {TAGS.map((t) => (
+            <SidebarItem
+              key={t.id}
+              to={`/tag/${t.id}`}
+              Icon={TagIcon}
+              text={t.name}
+            />
+          ))}
+          </ul>
+        </div>
+        </div>}
+      {(tagId) && 
         <div className={styles["tagged-notes"]}>
           <NotesList notes={taggedNotes} basePath={`/tag/${tagId}`} />
-
           {!isDesktop && noteId && (
             <div className={styles["overlay"]}>
               <Outlet />
             </div>
           )}
-
           {isDesktop && noteId && <Outlet />}
         </div>
       }
