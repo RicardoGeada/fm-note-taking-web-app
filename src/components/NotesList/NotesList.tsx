be9d500fc4f92990dcdbb4b9cb973b4644efcf69
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-10-15 18:59:20 +0200

    feat(notes-list): add tag  info text

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -6,6 +6,7 @@ import { useMediaQuery } from "react-responsive";
 import type { Note } from "../../types/note";
 import { useCurrentRouteInfo } from "../../hooks/useCurrentRouteInfo";
 import { Link } from "react-router-dom";
+import { TAGS } from "../../dummy-notes";
 
 type NotesListProps = {
   notes: Note[];
@@ -15,7 +16,8 @@ type NotesListProps = {
 
 export default function NotesList({ notes, basePath }: NotesListProps) {
   const isDesktop = useMediaQuery({ minWidth: 1080 });
-  const { title, isArchivedRoute } = useCurrentRouteInfo();
+  const { title, isArchivedRoute, isTagRoute, tagId } = useCurrentRouteInfo();
+  const tagName = TAGS.find(t => t.id === tagId)?.name;
 
   return (
     <div className={styles["notes-list-wrapper"]}>
@@ -34,6 +36,7 @@ export default function NotesList({ notes, basePath }: NotesListProps) {
       </button>
 
       {isArchivedRoute && <p>All your archived notes are stored here. You can restore or delete them anytime.</p>}
+      {(isTagRoute && tagName) && <p>All notes with the "{tagName}" tag are shown here.</p>}
 
       {notes.length === 0 && (
         <div className={styles["notes-list__empty-notification"]}>
