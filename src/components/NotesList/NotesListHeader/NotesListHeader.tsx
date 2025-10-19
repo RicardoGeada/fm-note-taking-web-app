e1fa14fc31b8951249dd9eb6755918dd74ff6c58
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-10-19 22:56:42 +0200

    refactor(noteslist): add noteslistheader

---
+++ src/components/NotesList/NotesListHeader/NotesListHeader.tsx
@@ -0,0 +1,19 @@
+import styles from "./NoteListHeader.module.scss"
+import ArrowLeftIcon from "../../../assets/images/icon-arrow-left.svg?react";
+import { useNavigate } from "react-router-dom";
+
+export default function NotesListHeader() {
+  const navigate = useNavigate();  
+
+  return (
+    <header>
+      <button
+        className={styles["notes-list__controls-button"]}
+        onClick={() => navigate(".")}
+      >
+        <ArrowLeftIcon />
+        <span>Go Back</span>
+      </button>
+    </header>
+  );
+}
