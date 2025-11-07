0e27c6b017a29292522ee58972ff8ac1a77ccbff
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-11-07 22:50:37 +0100

    feat(firestore): add FireStoreContext, initialize user

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,8 +1,17 @@
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, confirmPasswordReset } from "firebase/auth";
+import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
 
 export const signUp = async (email : string, password: string) => {
-    return createUserWithEmailAndPassword(auth, email, password);
+    const result = await createUserWithEmailAndPassword(auth, email, password);
+
+    // create document for user in database
+    const userRef = doc(db, "users", result.user.uid);
+    await setDoc(userRef, {
+        createdAt: serverTimestamp(),
+    });
+
+    return result
 };
 
 export const signIn = async (email: string, password: string) => {
@@ -12,7 +21,16 @@ export const signIn = async (email: string, password: string) => {
 export const signInWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     const result = await signInWithPopup(auth, provider);
-    //result.user
+
+    // create document for user in database
+    const userRef = doc(db, "users", result.user.uid);
+    const userDoc = await getDoc(userRef);
+    if (!userDoc.exists()) {
+      await setDoc(userRef, {
+        createdAt: serverTimestamp(),
+      });
+    }
+
     return result
 }
 
