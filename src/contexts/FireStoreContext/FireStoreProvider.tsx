e551b3da8a67d87cbb775596e9b15644b3ca0d01
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-11-08 23:20:37 +0100

    feat(firestore): snaphot notes

--- src/contexts/FireStoreContext/FireStoreProvider.tsx
+++ src/contexts/FireStoreContext/FireStoreProvider.tsx
@@ -1,26 +1,46 @@
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { FireStoreContext } from "./FireStoreContext";
 import type { Note } from "../../types/note";
 
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 interface FireStoreProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export function FireStoreProvider({children}: FireStoreProviderProps) {
+export function FireStoreProvider({ children }: FireStoreProviderProps) {
+  const { currentUser } = useAuthContext();
+  const [notes, setNotes] = useState<Note[]>([]);
 
-    const [notes, setNotes] = useState<Note[] | null>(null);
-    const [currentNote, setCurrentNote] = useState<Note | null>(null);
-
-
-    const value = {
-        notes,
-        currentNote
+  useEffect(() => {
+    if(!currentUser) {
+        setNotes([]);
+        return
     }
 
-    return (
-        <FireStoreContext.Provider value={value}>
-            {children}
-        </FireStoreContext.Provider>
-    )
+    const notesQuery = query(collection(db, "users", currentUser.uid, "notes"));
+    const unsubscribe = onSnapshot(notesQuery, (snapshot) => {
+        const loadedNotes: Note[] = [];
+        snapshot.forEach((doc) => {
+            loadedNotes.push({ id: doc.id, ...doc.data() } as Note)
+        });
+
+        setNotes(loadedNotes);
+    })
+
+    return () => unsubscribe();
+  }, [currentUser]);
+
+
+  const value = {
+    notes
+  };
+
+  return (
+    <FireStoreContext.Provider value={value}>
+      {children}
+    </FireStoreContext.Provider>
+  );
 }
--- src/pages/main/Home/Home.tsx
+++ src/pages/main/Home/Home.tsx
@@ -3,15 +3,18 @@ import MobileLayout from "../../../layouts/Mobile/MobileLayout";
 import DesktopLayout from "../../../layouts/Desktop/DesktopLayout";
 import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../../hooks/useAuthContext";
+import { FireStoreProvider } from "../../../contexts/FireStoreContext/FireStoreProvider";
 
 function Home() {
-  const isDesktop = useMediaQuery({minWidth: 1080});
+  const isDesktop = useMediaQuery({ minWidth: 1080 });
   const { userLoggedIn } = useAuthContext();
 
-  if(!userLoggedIn) return <Navigate to="/login" />
+  if (!userLoggedIn) return <Navigate to="/login" />;
 
   return (
-    isDesktop ? <DesktopLayout /> : <MobileLayout />
+    <FireStoreProvider>
+      {isDesktop ? <DesktopLayout /> : <MobileLayout />}
+    </FireStoreProvider>
   );
 }
 
