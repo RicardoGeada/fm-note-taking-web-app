2fe49fcb71d14a4f76c34fd7a1825d94acd6ff83
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-11-11 15:05:11 +0100

    style: fix minor style issues

--- src/pages/main/Tags/Tags.tsx
+++ src/pages/main/Tags/Tags.tsx
@@ -1,6 +1,5 @@
 import styles from "./Tags.module.scss";
 import NotesList from "../../../components/NotesList/NotesList";
-import { TAGS } from "../../../dummy-notes";
 import SidebarItem from "../../../components/SidebarItem/SidebarItem";
 import TagIcon from "../../../assets/images/icon-tag.svg?react";
 import MainContentWrapper from "../../../components/MainContentWrapper/MainContentWrapper";
@@ -9,7 +8,7 @@ import { useCurrentRouteInfo } from "../../../hooks/useCurrentRouteInfo";
 
 export default function Tags() {
   const { tagId } = useCurrentRouteInfo();
-  const { notes } = useFireStoreContext();
+  const { notes, tags } = useFireStoreContext();
   const taggedNotes = tagId
     ? notes.filter((note) => note.tags.includes(tagId))
     : [];
@@ -20,12 +19,12 @@ export default function Tags() {
         <div className={styles["tag-list-wrapper"]}>
           <h1 className={styles["tag-list__headline"]}>Tags</h1>
           <ul className={styles["tag-list"]}>
-            {TAGS.map((t) => (
+            {tags.map((t) => (
               <SidebarItem
-                key={t.id}
-                to={`/tag/${t.id}`}
+                key={t}
+                to={`/tag/${t}`}
                 Icon={TagIcon}
-                text={t.name}
+                text={t}
               />
             ))}
           </ul>
