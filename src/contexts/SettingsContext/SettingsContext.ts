dc5f12eb75d9abeaefea4469fa9ac916b24a8e6d
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-11-23 19:47:04 +0100

    feat(firestore): user settings

--- src/contexts/SettingsContext/SettingsContext.ts
+++ src/contexts/SettingsContext/SettingsContext.ts
@@ -3,6 +3,7 @@ import { createContext } from "react";
 export interface SettingsContextType {
     colorTheme: "light-mode" | "dark-mode" | "system",
     fontTheme: "sans-serif" | "serif" | "monospace",
+    isLoadingSettings: boolean,
 }
 
 export const SettingsContext = createContext<SettingsContextType | undefined>(undefined);
--- src/contexts/SettingsContext/SettingsProvider.tsx
+++ src/contexts/SettingsContext/SettingsProvider.tsx
@@ -1,22 +1,56 @@
-import type { ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { SettingsContext } from "./SettingsContext";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
-interface SettingsProviderProps {
-    children: ReactNode;
+interface UserSettings {
+  colorTheme: "light-mode" | "dark-mode" | "system";
+  fontTheme: "sans-serif" | "serif" | "monospace";
 }
 
-export function SettingsProvider({children}: SettingsProviderProps) {
-    const colorTheme : "light-mode" | "dark-mode" | "system" = "light-mode";
-    const fontTheme : "sans-serif" | "serif" | "monospace" = "sans-serif";
+interface SettingsProviderProps {
+  children: ReactNode;
+}
 
-    const value = {
-        colorTheme,
-        fontTheme,
+const defaultSettings: UserSettings = {
+  colorTheme: "system",
+  fontTheme: "sans-serif",
+};
+
+export function SettingsProvider({ children }: SettingsProviderProps) {
+  const { currentUser } = useAuthContext();
+  const [settings, setSettings] = useState<UserSettings>(defaultSettings);
+  const [isLoadingSettings, setIsLoadingSettings] = useState(true);
+
+  useEffect(() => {
+    if (!currentUser) {
+        setSettings(defaultSettings);
+        setIsLoadingSettings(false);
+        return;
     }
 
-    return (
-        <SettingsContext.Provider value={value}>
-            {children}
-        </SettingsContext.Provider>
-    )
+    const docRef = doc(db, "users", currentUser.uid, "settings");
+    const unsubscribe = onSnapshot(docRef, (snapshot) => {
+        if(snapshot.exists()) {
+            setSettings(snapshot.data() as UserSettings);
+        } else {
+            setSettings(defaultSettings);
+        }
+        setIsLoadingSettings(false);
+    })
+
+    return () => unsubscribe();
+  }, [currentUser])
+
+  const value = {
+    ...settings,
+    isLoadingSettings,
+  };
+
+  return (
+    <SettingsContext.Provider value={value}>
+      {children}
+    </SettingsContext.Provider>
+  );
 }
